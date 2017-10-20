571afbd5422372b596714c15d4f12fcdb3f2363c
SometimesPie <30302344+SometimesPie@users.noreply.github.com>
2017-10-20 12:58:16 -0300

    Update rest.java

--- src/rest.java
+++ src/rest.java
@@ -8,9 +8,9 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 @Path("/")
 public class rest{
@@ -20,11 +20,19 @@ public class rest{
     @Produces(MediaType.APPLICATION_JSON)
     //Recibe un String de no más de 4 caracter en minúscula y devuelve el mismo String en mayúsculas.
     public Response doCaps(word message){
-	    if(!message.data.chars().allMatch(Character::isLetter) || message.data.length() > 4){
-	    	throw new WebApplicationException(400);
-	    }
-	    String r = "{\"code\": 200, \"descripcion\": \"OK\", \"data\": \"" + message.data.toUpperCase() + "\"}";
-	    return Response.ok(r).build();
+    	try {
+		    if(!message.data.chars().allMatch(Character::isLetter) || message.data.length() > 4){
+		    	String m = "{\"code\": 400, \"descripcion\": \"BAD REQUEST data must be all letters and no more than four characters\", \"data\": \"" + message.data + "\"}"; 
+		    	return Response.status(Status.BAD_REQUEST).entity(m).build();
+		    }
+		    String r = "{\"code\": 200, \"descripcion\": \"OK\", \"data\": \"" + message.data.toUpperCase() + "\"}";
+		    return Response.ok(r).build();
+    	}
+    	catch(Exception e){
+    		System.out.println(e);
+    		String m = "{\"code\": 500, \"descripcion\": \"INTERNAL SERVER ERROR oops algo exploto\", \"data\": \"" + message.data + "\"}";
+    		return Response.status(Status.INTERNAL_SERVER_ERROR).entity(m).build();
+    	}
     }
     
     @GET
@@ -32,16 +40,24 @@ public class rest{
     @Produces(MediaType.APPLICATION_JSON)
     //Recibe hora en formato HH:mm:ss y devuelve la fecha actual y la hora recibida en formato UTC ISODATE.
     public Response getTime(@QueryParam("value") String hora){
-    	SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-    	if(!hora.matches("([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]")){
-    		throw new WebApplicationException(400);
+    	try {
+	    	SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+	    	if(!hora.matches("([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]")){
+	    		String m = "{\"code\": 400, \"descripcion\": \"BAD REQUEST data must be in HH:mm:ss format\", \"data\": \"" + hora + "\"}"; 
+		    	return Response.status(Status.BAD_REQUEST).entity(m).build();
+	    	}
+	    	String[] time = hora.split(":");
+	    	Calendar today = Calendar.getInstance();
+	    	today.set(Calendar.HOUR_OF_DAY, Integer.parseInt(time[0]));
+	    	today.set(Calendar.MINUTE, Integer.parseInt(time[1]));
+	    	today.set(Calendar.SECOND, Integer.parseInt(time[2]));
+	   	    String r = "{\"code\": 200, \"descripcion\": \"OK\", \"data\": \"" + df.format(today.getTime()) + "\"}";
+	   	    return Response.ok(r).build();
     	}
-    	String[] time = hora.split(":");
-    	Calendar today = Calendar.getInstance();
-    	today.set(Calendar.HOUR_OF_DAY, Integer.parseInt(time[0]));
-    	today.set(Calendar.MINUTE, Integer.parseInt(time[1]));
-    	today.set(Calendar.SECOND, Integer.parseInt(time[2]));
-   	    String r = "{\"code\": 200, \"descripcion\": \"OK\", \"data\": \"" + df.format(today.getTime()) + "\"}";
-   	    return Response.ok(r).build();    	
-    }   
+    	catch(Exception e){
+    		System.out.println(e);
+    		String m = "{\"code\": 500, \"descripcion\": \"INTERNAL SERVER ERROR oops algo exploto\", \"data\": \"" + hora + "\"}";
+    		return Response.status(Status.INTERNAL_SERVER_ERROR).entity(m).build();
+    	}	
+    }
 }
